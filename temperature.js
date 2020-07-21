089bedb67c43e5992cbf3579911b6e65732fbb66
scumdestroy <35894688+scumdestroy@users.noreply.github.com>
2020-07-21 16:15:22 -0700

    Create temperature.js

---
+++ temperature.js
@@ -0,0 +1,9 @@
+// const don't change bruh
+const kelvin = 0
+// celsius like uh a diff number 
+const celsius = kelvin - 273
+// nine fifths plus thirty two
+let fahrenheit = celsius * (9/5) + 32
+
+fahrenheit = Math.floor(fahrenheit)
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit.')
