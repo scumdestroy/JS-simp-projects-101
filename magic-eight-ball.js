14154da89466c280d6d86d030986c009d6ac1309
scumdestroy <35894688+scumdestroy@users.noreply.github.com>
2020-07-21 16:14:20 -0700

    Create magic-eight-ball.js

---
+++ magic-eight-ball.js
@@ -0,0 +1,39 @@
+let userName = ""
+
+if (userName){
+  console.log("Hello, " + username);
+} else {
+  console.log("Hello")
+}
+
+const userQuestion = "Should I follow my instincts and become an adult baby?";
+  console.log(`Thou truly asked my wizardly magnificence: ${userQuestion} `)
+
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber) {
+  case 1:
+    console.log('Yes, bruh');
+    break;
+  case 2:
+    console.log('Absolutely');
+    break;
+  case 3:
+    console.log('I can\'t even see right now');
+    break;
+  case 4:
+    console.log('Eyes like marbled up at the moment');
+    break;
+  case 5:
+    console.log('lol nah');
+    break;
+  case 6:
+    console.log('My boys say you\'re all set, as in no');
+    break;
+  case 7:
+    console.log('yeah I really wouldn\'t eeeeh');
+    break;
+  case 8:
+    console.log('Are you a simp? The answer is yes!');
+    break;
+}
