3e91dbdf75e101ff36f5e290119c306807099fee
scumdestroy <35894688+scumdestroy@users.noreply.github.com>
2020-07-21 16:13:59 -0700

    Create dogyears.js

---
+++ dogyears.js
@@ -0,0 +1,11 @@
+var myAge = 6;
+// dats my age
+var earlyYears = 2;
+// it gone change
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(laterYears)
+let myAgeInDogYears = earlyYears + laterYears
+var myName = "Jann".toLowerCase()
+console.log('My name is ' + myName +'. I am '+ myAge + ' years old in human years which is ' + myAgeInDogYears +' years old in dog years.')
